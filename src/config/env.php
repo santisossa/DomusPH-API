4d42139a29d113252ceb127b84294995b2c6ecde
Santiago Sossa <santisossa67@gmail.com>
2019-02-20 23:51:23 -0500

    cambios en .env

--- src/config/env.php
+++ src/config/env.php
@@ -3,9 +3,9 @@
 define('DATABASE', [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'domusph',
-    'username' => 'root',
-    'password' => '',
+    'database' => 'domusph_domusph',
+    'username' => 'domusph_admin',
+    'password' => 'Dc@{$~w*$aj_',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
